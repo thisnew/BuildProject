ce33e181ee3b0629c354a86c83e769e897aa8d67
thisnew <nzy-666@126.com>
2018-06-11 18:50:36 +0800

    Update firstChackOut.sh

--- firstChackOut.sh
+++ firstChackOut.sh
@@ -2,15 +2,13 @@
 
 
 # updata svn code
-svn list https://10.129.33.214:8443/svn/jrbsproweb/trunk --username $1 --password $2
+svn list https://---/svn/jrbsproweb/trunk --username $1 --password $2
 
 read -p "输入项目名称:" project
 
-svn co https://10.129.33.214:8443/svn/jrbsproweb/trunk/$project --$1 niuzongyuan2016 --password $2
+svn co https://---/svn/jrbsproweb/trunk/$project --$1 niuzongyuan2016 --password $2
 #cd $1
 #svn update
 # read file path
 # build code
 # zip
-#https://DESKTOP-TVO89QL/svn/allpro/trunk/jrbs_report
-#https://jrbspro.jinrongboshi.com:8443/svn/jrbsproweb/trunk/jrbs_standard_v4.0
